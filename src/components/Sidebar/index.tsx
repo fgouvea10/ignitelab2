5ceafce458e715f8dc75e4f4497b0b07327d0374
fgouvea10 <felipegalvesouvea@gmail.com>
2022-06-21 20:50:57 -0300

    feat: create sidebar component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
+
+import { Lesson } from "../Lesson";
+import "./styles.css";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
+}
+
+export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
+  return (
+    <aside className="sidebar">
+      <span className="class-schedule">Cronograma de aulas</span>
+
+      <div className="lessons">
+        {data?.lessons.map((lesson) => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            availableAt={new Date(lesson.availableAt)}
+            type={lesson.lessonType}
+          />
+        ))}
+      </div>
+    </aside>
+  );
+}
