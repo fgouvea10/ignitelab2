1423ed94fc0186f74adb35a2a5d3621311336273
fgouvea10 <felipegalvesouvea@gmail.com>
2022-06-22 21:30:20 -0300

    feat: add routing between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import Event from "./pages/Event";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+import { client } from "./lib/apollo";
+
+import { Router } from "./Router";
+
+import "@vime/core/themes/default.css";
+import "@vime/core/themes/light.css";
+
+import "./styles/global.css";
 
 export default function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloProvider } from "@apollo/client";
 
 import App from "./App";
-import { client } from "./lib/apollo";
-
-import '@vime/core/themes/default.css';
-import '@vime/core/themes/light.css';
-
-import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 );
